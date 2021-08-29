eb71e2a9dd2482436d2ba2adbf1f28ef8fb67b96
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-30 03:21:50 +0530

    Added: MoodAnalyserTest tests mood

--- src/com/exceptions/MoodAnalyserTest.java
+++ src/com/exceptions/MoodAnalyserTest.java
@@ -1,9 +1,22 @@
 package com.exceptions;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyserTest {
 
-    public static void main(String[] args) {
-	    //
+    public String analyseMood(String mood){
+        if(mood.contains("sad")){
+            return "SAD";
+        }
+        return "HAPPY";
+    }
 
+    @Test
+    public void MoodAnalyser() throws Exception{
+        MoodAnalyserTest moodAnalyser = new MoodAnalyserTest();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
     }
 }
